0b8e9fad7a7a170eef33a69cff01de040a1fbb56
minhquansicula <wuanminh777@gmail.com>
2026-04-17 23:57:17 +0700

    Initial commit: Khởi tạo project Spring Boot với các dependencies cơ bản

---
+++ src/main/java/com/quan/deadline_pet_be/DeadlinePetBeApplication.java
@@ -0,0 +1,13 @@
+package com.quan.deadline_pet_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadlinePetBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadlinePetBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quan/deadline_pet_be/DeadlinePetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan.deadline_pet_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadlinePetBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
